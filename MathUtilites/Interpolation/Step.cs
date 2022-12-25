6ab62fb849b13a87fae18782a19e76842c95cdf7
Виталий Крюков <mr.vitalik.kr@yandex.ru>
2022-12-25 20:22:22 +0500

    Step Overload

--- MathUtilites/Interpolation/Step.cs
+++ MathUtilites/Interpolation/Step.cs
@@ -9,5 +9,14 @@
 
             return (x - x0) / (x1 - x0);
         }
+
+        public static double Find(double x0, double x1, double x) 
+            => (double)Find((decimal)x0, (decimal)x1, (decimal)x);
+
+        public static double Find(float x0, float x1, float x)
+            => (double)Find((decimal)x0, (decimal)x1, (decimal)x);
+
+        public static double Find(int x0, int x1, int x)
+            => (double)Find((decimal)x0, (decimal)x1, (decimal)x);
     }
 }
