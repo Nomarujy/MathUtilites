fe4c11e7e9e39f4e59adee47d27f01140a8e93a4
Виталий Крюков <mr.vitalik.kr@yandex.ru>
2023-01-29 18:25:44 +0500

    Remove IsRightAngled Method

--- MathUtilites/Geometry/2D/Triangle.cs
+++ MathUtilites/Geometry/2D/Triangle.cs
@@ -1,27 +1,73 @@
-﻿namespace MathUtilites.Geometry
+﻿using MathUtilites.Geometry.Entity;
+
+namespace MathUtilites.Geometry
 {
     public class Triangle : IFigure
     {
-        public Triangle(double a = 0, double b = 0, double c = 0)
+        public Triangle(double sideA = 3, double sideB = 4, double sideC = 5)
         {
-            SideA = a;
-            SideB = b;
-            SideC = c;
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
         }
 
-        public double SideA = 0;
-        public double SideB = 0;
-        public double SideC = 0;
-
-        public bool IsRightAngled => IsRightAndled();
-
-        private bool IsRightAndled()
+        public Triangle(double sideA, double sideB, double sideC, Vector2D center) : this(sideA, sideB, sideC)
         {
-            var maxSide = Math.Max(SideA, Math.Max(SideB, SideC));
-            var minSide = Math.Min(SideA, Math.Min(SideB, SideC));
-            var avgSide = Math.Max(SideA, Math.Min(SideB, SideC));
+            Center= center;
+        }
 
-            return Math.Pow(maxSide, 2) == Math.Pow(minSide, 2) + Math.Pow(avgSide, 2);
+        public Vector2D Center { get; set; }
+
+        private double _sideA;
+        private double _sideB;
+        private double _sideC;
+
+        public double SideA
+        {
+            get => _sideA;
+            set
+            {
+                if (value > 0)
+                {
+                    _sideA = value;
+                }
+            }
+        }
+
+        public double SideB
+        {
+            get => _sideB;
+            set
+            {
+                if (value > 0)
+                {
+                    _sideB = value;
+                }
+            }
+        }
+
+        public double SideC
+        {
+            get => _sideC;
+            set
+            {
+                if (value > 0)
+                {
+                    _sideC = value;
+                }
+            }
+        }
+
+        public bool IsRightAngled
+        {
+            get
+            {
+                var maxSide = Math.Max(SideA, Math.Max(SideB, SideC));
+                var minSide = Math.Min(SideA, Math.Min(SideB, SideC));
+                var avgSide = Math.Max(SideA, Math.Min(SideB, SideC));
+
+                return Math.Pow(maxSide, 2) == Math.Pow(minSide, 2) + Math.Pow(avgSide, 2);
+            }
         }
 
         public double Area => FindArea();
