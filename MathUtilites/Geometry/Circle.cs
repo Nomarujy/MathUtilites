90f1182b9134c1fadc7755ff7c37646ae5909915
Виталий Крюков <mr.vitalik.kr@yandex.ru>
2023-01-29 07:56:01 +0500

    Circle Oint By degrees +  Tests

--- MathUtilites/Geometry/Circle.cs
+++ MathUtilites/Geometry/Circle.cs
@@ -16,9 +16,9 @@ namespace MathUtilites.Geometry
             }
         }
 
-        public Circle(double radius = 1) : this(Vector2D.Zero, radius) { }
+        public Circle(double radius = 1) : this(radius, Vector2D.Zero) { }
 
-        public Circle(Vector2D center, double radius)
+        public Circle(double radius, Vector2D center)
         {
             Center = center;
             Radius = radius;
@@ -27,13 +27,28 @@ namespace MathUtilites.Geometry
         public override double Area => Radius * Math.PI * Radius;
         public override double Perimetr => 2 * Math.PI * Radius;
 
-        public Vector2D PointByAnglge(double angle)
+        public Vector2D PointByDegrees(double degrees)
         {
+            var radian = ConvertToRadian(degrees);
+
+            return PointByRadian(radian);
+        }
+
+        public Vector2D PointByRadian(double radian)
+        {
+            var cos = Math.Cos(radian); 
+            var si = Math.Sin(radian); 
+
             return new Vector2D()
             {
-                X = Center.X + Math.Cos(angle) * Radius,
-                Y = Center.X + Math.Sin(angle) * Radius,
+                X = Center.X + Math.Cos(radian) * Radius,
+                Y = Center.X + Math.Sin(radian) * Radius,
             };
+        }
+
+        public static double ConvertToRadian(double degrees)
+        {
+            return (Math.PI / 180) * degrees;
         }
     }
 }
