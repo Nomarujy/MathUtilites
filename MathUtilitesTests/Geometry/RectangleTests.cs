8d5cf009ef0ca53744d83df894518460277f5ea0
Виталий Крюков <mr.vitalik.kr@yandex.ru>
2023-02-01 07:52:56 +0500

    Tests for rectangle and square

---
+++ MathUtilitesTests/Geometry/RectangleTests.cs
@@ -0,0 +1,65 @@
+﻿using MathUtilites.Geometry;
+using MathUtilites.Geometry.Entity;
+
+namespace MathUtilitesTests.Geometry
+{
+    public class RectangleTests
+    {
+        [Fact]
+        public void Perimetr()
+        {
+            var rec = new Rectangle(1, 4);
+            var expected = 10;
+
+            var result = rec.Perimetr;
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Area()
+        {
+            var rec = new Rectangle(4, 4);
+            var expected = 16;
+
+            var result = rec.Area;
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetCoordinates_4Points()
+        {
+            var rec = new Rectangle(2, 2);
+
+            var result = rec.GetCoordinates();
+
+            Assert.Equal(4, result.Length);
+
+            Assert.True(result[0].Equals(new Vector2D(1, 1), 0.00001));
+            Assert.True(result[1].Equals(new Vector2D(-1, 1), 0.00001));
+            Assert.True(result[2].Equals(new Vector2D(-1, -1), 0.00001));
+            Assert.True(result[3].Equals(new Vector2D(1, -1), 0.00001));
+        }
+
+        [Fact]
+        public void Width_NegativeValue_NotApplied()
+        {
+            var rec = new Rectangle(1, 1);
+
+            rec.Width = -1;
+
+            Assert.NotEqual(-1, rec.Width);
+        }
+
+        [Fact]
+        public void Height_NegativeValue_NotApplied()
+        {
+            var rec = new Rectangle(1, 1);
+
+            rec.Height = -1;
+
+            Assert.NotEqual(-1, rec.Width);
+        }
+    }
+}
---
+++ MathUtilitesTests/Geometry/SquareTests.cs
@@ -0,0 +1,49 @@
+﻿using MathUtilites.Geometry;
+
+namespace MathUtilitesTests.Geometry
+{
+    public class SquareTests
+    {
+        [Fact]
+        public void Width_Negative_NotApplied()
+        {
+            var square = new Square(2);
+
+            square.Width = -1;
+
+            Assert.NotEqual(-1, square.Width);
+        }
+
+        [Fact]
+        public void Height_Negative_NotApplied()
+        {
+            var square = new Square(2);
+
+            square.Height = -1;
+
+            Assert.NotEqual(-1, square.Height);
+        }
+
+        [Fact]
+        public void Height_ChangeAllSides()
+        {
+            var square = new Square(2);
+
+            square.Height = 10;
+
+            Assert.Equal(10, square.Width);
+            Assert.Equal(10, square.Height);
+        }
+
+        [Fact]
+        public void Width_ChangeAllSides()
+        {
+            var square = new Square(2);
+
+            square.Width = 10;
+
+            Assert.Equal(10, square.Width);
+            Assert.Equal(10, square.Height);
+        }
+    }
+}
